06c308c72236960a2e69aa75a4b05c8c0f8a0b4a
vijaygopalgg <vijaygopal.v@gmail.com>
2020-08-04 18:02:53 -0700

    Create Hello.java

---
+++ Java Projects/HelloWorld/src/Hello.java
@@ -0,0 +1,11 @@
+
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello,Vijay");
+        System.out.println("Hello,World");
+        int salary_num = 5;
+        System.out.println(salary_num);
+     }
+}
+
