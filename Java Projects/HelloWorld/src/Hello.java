e24ac3a73f8a99c4348b7b03cb0695c7e0985b38
vijaygopalgg <vijaygopal.v@gmail.com>
2020-08-10 08:12:27 -0700

    Update Hello.java

--- Java Projects/HelloWorld/src/Hello.java
+++ Java Projects/HelloWorld/src/Hello.java
@@ -1,11 +1,10 @@
-
-
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello,Vijay");
         System.out.println("Hello,World");
-        int salary_num = 5;
-        System.out.println(salary_num);
+        int salary_num = 10+5 + (2*10);
+        int my_bonus = (salary_num * 110);
+        System.out.println(my_bonus);
      }
 }
 
